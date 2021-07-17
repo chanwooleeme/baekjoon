dabb9216ef3b44af4c2fda3713b59213efa439da
chanwooleeme <upup1810@gmail.com>
2021-07-17 21:02:31 +0900

    [MOD] 별 찍기 - 1

--- 단계별 문제/3.for문/2438/main.py
+++ 단계별 문제/3.for문/2438/main.py
@@ -1,8 +1,3 @@
-import sys
-
-n = int(sys.stdin.readline())
-
-for i in range(n):
-	for j in range(i + 1):
-		print("*", end='')
-	print("")
+n = int(input())
+for i in range(1, n + 1):
+	print("*" * i)
