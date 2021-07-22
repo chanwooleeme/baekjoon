1f3f024b5f6703067da86057226ad8a00f2d5eb5
chanwooleeme <upup1810@gmail.com>
2021-07-22 22:32:26 +0900

    [ADD] 크로아티아 알파벳

---
+++ 단계별 문제/7.문자열/2941/main.py
@@ -0,0 +1,15 @@
+s = input()
+li = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+cnt = 0
+
+while len(s) > 0:
+	flag = True
+	for i in li:
+		if s[0:len(i)] == i:
+			s = s[len(i):]
+			flag = False
+			cnt += 1
+	if flag:
+		s = s[1:]
+		cnt += 1
+print(cnt)
