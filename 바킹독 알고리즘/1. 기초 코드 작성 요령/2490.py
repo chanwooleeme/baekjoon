f48509cba70669d38e36e6178922da095ea78af3
chanwooleeme <upup1810@gmail.com>
2021-09-01 21:10:38 +0900

    [ADD] 윷놀이

---
+++ 바킹독 알고리즘/1. 기초 코드 작성 요령/2490.py
@@ -0,0 +1,12 @@
+for i in range(3) :
+	result = sum(list(map(int, input().split())))
+	if result == 0 :
+		print("E")
+	elif result == 1 :
+		print("C")
+	elif result == 2 :
+		print("B")
+	elif result == 3 :
+		print("A")
+	elif result == 4 :
+		print("D")
