ea57e3d6601c6c21fa3c7674aad49b2645996327
chanwooleeme <upup1810@gmail.com>
2021-10-04 23:13:06 +0900

    Update 8.py

--- python-for-coding-test/2/8.py
+++ python-for-coding-test/2/8.py
@@ -1,13 +1,13 @@
-s = list(input())
-num_list = list()
-chr_list = list()
-for item in s:
-	if '0' <= item <= '9':
-		num_list.append(int(item))
+data = input()
+result = []
+value = 0
+
+for x in data:
+	if x.isalpha():
+		result.append(x)
 	else:
-		chr_list.append(item)
-result = ""
-chr_list.sort()
-for chr in chr_list:
-	result += chr
-print(result+str(sum(num_list)))
+		value += int(x)
+result.sort()
+if value != 0:
+	result.append(str(value))
+print(''.join(result))
