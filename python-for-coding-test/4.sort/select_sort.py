96e2e3ec8c2675f2fd7aeb11d14e24360363621e
chanwooleeme <upup1810@gmail.com>
2021-10-05 22:50:06 +0900

    Create select_sort.py

---
+++ python-for-coding-test/4.sort/select_sort.py
@@ -0,0 +1,16 @@
+"""
+가장 작은 데이터를 선택해 맨 앞에 있는 데이터와 바꾸는 것을 반복
+"""
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def my_select_sort():
+	global array
+	for i in range(len(array)):
+		min_index = i
+		for j in range(i + 1, len(array)):
+			if array[min_index] > array[j]:
+				min_index = j
+		array[i], array[min_index] = array[min_index], array[i]
+
+my_select_sort()
+print(array)
