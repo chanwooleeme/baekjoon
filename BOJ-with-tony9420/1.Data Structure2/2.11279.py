55dabe30ee7b104ca901657fb968e87d47db8ba5
chanwooleeme <upup1810@gmail.com>
2021-12-10 05:07:08 +0900

    최대 힙

---
+++ BOJ-with-tony9420/1.Data Structure2/2.11279.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N = int(input())
+heap = []
+for _ in range(N):
+    a = int(input())
+    if a == 0:
+        if heap:
+            print(-heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, -a)
+        
