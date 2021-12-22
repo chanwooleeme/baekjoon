0c7f18bfe63fad6a07bd08a2c530768cdd8ad79a
chanwooleeme <upup1810@gmail.com>
2021-12-23 00:40:27 +0900

    ZOAC 3

--- BOJ-with-tony9420/8.Implementation/15.20436.py
+++ BOJ-with-tony9420/8.Implementation/15.20436.py
@@ -1,31 +1,31 @@
-keyboard_init = [list("qwertyuiop"), list("asdfghjkl"), list("zxcvbnm")]
-keyboard = {}
-for i in range(len(keyboard_init)):
-    for j in range(len(keyboard_init[i])):
-        keyboard[keyboard_init[i][j]] = [i, j]
-def distance(l, r, goal):
-        lx, ly = keyboard[l]
-        rx, ry = keyboard[r]
-        goalx, goaly = keyboard[goal]
-        l_distance = ((lx - goalx) ** 2 ** 0.5) + ((ly - goaly) ** 2 ** 0.5)
-        print(l_distance)
-        r_distance = ((rx - goalx) ** 2 ** 0.5) + ((ry - goaly) ** 2 ** 0.5)
-        print(r_distance)
-        if l_distance < r_distance:
-            l = goal
-            return r, l, r_distance
-        else:
-            r = goal
-            return r, l, l_distance
+l_keyboard_init = [list("qwert"), list("asdfg"), list("zxcv")]
+r_keyboard_init = [list(" yuiop"), list(" hjkl "), list("bnm   ")]
+l_keyboard = {}
+r_keyboard = {}
+for i in range(len(l_keyboard_init)):
+    for j in range(len(l_keyboard_init[i])):
+        l_keyboard[l_keyboard_init[i][j]] = [i, j]
+
+for i in range(len(r_keyboard_init)):
+    for j in range(len(r_keyboard_init[i])):
+        r_keyboard[r_keyboard_init[i][j]] = [i, j]
+        
+def distance(finger, keyboard, goal):
+    x, y = keyboard[finger]
+    goal_x, goal_y = keyboard[goal]
+    distance = int((((x - goal_x) ** 2) ** 0.5) + (((y - goal_y) ** 2) ** 0.5))
+    return distance
         
 l, r = input().split()
 s = input()
 cnt = 0
 for item in s:
-    l_tmp, r_tmp, cnt_tmp = distance(l, r, item)
-    l = l_tmp
-    r = r_tmp
-    cnt += cnt_tmp + 1
-
+    if item in l_keyboard:
+        cnt += distance(l, l_keyboard, item) + 1
+        l = item
+    else:
+        cnt += distance(r, r_keyboard, item) + 1
+        r = item
+        
 print(cnt)
     
