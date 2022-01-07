2d46719fb8731aa8df0264db989d29a82a02d2d5
chanwooleeme <upup1810@gmail.com>
2022-01-07 16:07:32 +0900

    인구이동(추가 수정중)

--- BOJ-with-tony9420/9.Graph Traversal/11.16234.py
+++ BOJ-with-tony9420/9.Graph Traversal/11.16234.py
@@ -10,31 +10,46 @@ def validate(x, y):
         return True
     return False
 
-def bfs():
-    distance = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-    q = deque((0, 0))
-    change_idx = []
-    flag = False
-    for i in range(N):
-        for j in range(N):
-            if not visited[i][j]:
-                visited[i][j] = True
-                while q:
-                    x, y = q.popleft()
-                    for d in distance:
-                        nx = d[0] + x
-                        ny = d[1] + y
-                        if validate(nx, ny):
-                            diff = abs(arr[x][y] - arr[nx][ny])
-                            if not visited[nx][ny] and L <= diff <= R:
-                                visited[nx][ny] = True
-                                flag = True
+def bfs(i, j):
+    q = deque()
+    distance = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    visited[i][j] = False
+    q.append((i, j))
+    country_list = [(i, j)]
+    population = arr[i][j]
+    country = 1
+    while q:
+        x, y = q.popleft()
+        visited[x][y] = True
+        for d in distance:
+            nx = d[0] + x
+            ny = d[1] + y
+            if validate(nx, ny):
+                diff = abs(arr[x][y] - arr[nx][ny])
+                if not visited[nx][ny] and L <= diff <= R:
+                    visited[nx][ny] = True
+                    country_list.append((nx, ny))
+                    population += arr[nx][ny]
+                    country += 1
+                    q.append((nx, ny))
+                    
+    if country > 1:
+        for item in country_list:
+            x, y = item
+            arr[x][y] = population // country
+            print(arr[x][y])
+        return True
     return False
-
+        
 #입력
 N, L, R = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 visited = [[False] * N for _ in range(N)]
 cnt = 0
-while bfs():
-    cnt += 1
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j]:
+            if bfs(i, j):
+                cnt += 1
+
+print(cnt)
