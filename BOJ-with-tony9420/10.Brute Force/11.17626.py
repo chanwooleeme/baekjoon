b2e269403366dab83c2e95586261d62ce055ba55
chanwooleeme <upup1810@gmail.com>
2022-03-09 20:48:56 +0900

    Four Squares

---
+++ BOJ-with-tony9420/10.Brute Force/11.17626.py
@@ -0,0 +1,12 @@
+import sys
+n = int(input())
+
+dp = [0] * (n + 1)
+dp[1] = 1
+for i in range(1, n + 1):
+    dp[i] = dp[1] + dp[i - 1]
+    j = 2
+    while j*j <= i:
+        dp[i] = min(dp[i], 1 + dp[i - j * j])
+        j += 1
+print(dp[n])
