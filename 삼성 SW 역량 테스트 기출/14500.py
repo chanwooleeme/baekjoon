f989ed4ca844d0bd6adbe46e52cf30ca08fa25f0
chanwooleeme <upup1810@gmail.com>
2022-04-30 22:02:36 +0900

    테트로미노

--- 삼성 SW 역량 테스트 기출/14500.py
+++ 삼성 SW 역량 테스트 기출/14500.py
@@ -2,57 +2,40 @@ import sys
 
 input = sys.stdin.readline
 
+def dfs(x, y, cnt, result):
+    global ans
+    if ans >= result + max_v * (3 - cnt):
+        return
+    if cnt == 3:
+        ans = max(ans, result)
+        return
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >= N or ny >= M:
+            continue
+        if not visited[nx][ny]:
+            if cnt == 1:
+                visited[nx][ny] = True
+                dfs(x, y, cnt + 1, result + arr[nx][ny])
+                visited[nx][ny] = False    
+            visited[nx][ny] = True
+            dfs(nx, ny, cnt + 1, result + arr[nx][ny])
+            visited[nx][ny] = False
+    
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+max_v = max(map(max, arr))
+visited = [[False for _ in range(M)] for _ in range(N)]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0 ,1]
+ans = 0
 
-tet_line = [
-    [[0, 0], [0, 1], [0, 2], [0, 3]], 
-    [[0, 0], [1, 0], [2, 0], [3, 0]]
-]
-
-tet_square = [
-    [[0, 0], [0, 1], [1, 0], [1, 1]]
-    ]
-
-tet_L = [
-    [[0, 0], [1, 0], [2, 0], [2, 1]],
-    [[0, 0], [0, 1], [0, 2], [1, 0]],
-    [[0, 0], [0, 1], [1, 1], [2, 1]],
-    [[0, 2], [1, 0], [1, 1,],[1, 2]],
-    [[0, 1], [1, 1], [2, 0], [2, 1]],
-    [[0, 0], [1, 0], [1, 1], [1, 2]],
-    [[0, 0], [0, 1], [1, 0], [2, 0]],
-    [[0, 0], [0, 1], [0, 2], [1, 2]]
-    ]
-
-tet_jigzag = [
-    [[0, 0], [1, 0], [1, 1], [2, 1]],
-    [[0, 1], [0, 2], [1, 0], [1, 1]],
-    [[0, 1], [1, 0], [1, 1], [2, 0]],
-    [[0, 0], [0, 1], [1, 1], [1, 2]]
-]
-
-tet_bolok = [
-    [[0, 0], [0, 1], [0, 2], [1, 1]],
-    [[0, 1], [1, 0], [1, 1], [2, 1]],
-    [[0, 1], [1, 0], [1, 1], [1, 2]],
-    [[0, 0], [1, 0], [1, 1], [2, 0]]
-]
-
-tet_family = [tet_line, tet_square, tet_L, tet_jigzag, tet_bolok]
-
-result = 0
 for i in range(N):
     for j in range(M):
-        for tet in tet_family:
-            for shape in tet:
-                tet_val = 0
-                for d in shape:
-                    x, y = i + d[0], j + d[1]
-                    if x < 0 or y < 0 or x >= N or y >= M:
-                        continue
-                    tet_val += arr[x][y]
-                result = max(result, tet_val)
+        visited[i][j] = True
+        dfs(i, j, 0, arr[i][j])
+        visited[i][j] = False
 
-print(result)
+print(ans)
         
