d49c549357f219de01f59cd6d62a78cf00cc7046
chanwooleeme <upup1810@gmail.com>
2022-06-29 19:09:13 +0900

    더 맵게

---
+++ programmers/level2/더 맵게.py
@@ -0,0 +1,18 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    cnt = 0
+    while scoville[0] < K:
+        if not scoville:
+            return -1
+        a = heapq.heappop(scoville)
+        if not scoville:
+            return -1
+        b = heapq.heappop(scoville)
+    
+        heapq.heappush(scoville, a + (b * 2))
+        cnt += 1
+    return cnt
+
+print(solution([1, 2, 3, 9, 10, 12], 7))
